8da9557eb508666811374880c79b5c6ebc586819
Ariyana Chatterjee <chatterjee.ariyana@gmail.com>
2025-09-21 15:41:18 +0530

    Add files via upload

---
+++ 5. Swapping 2 integers.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+WAPC to input two integers and display the contents after swapping.
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter two numbers:");
+    scanf("%d %d", &num1, &num2);
+
+    num1= num1 + num2;
+    num2= num1 - num2;
+    num1 = num1 - num2;
+
+    printf("The two numbers, after swapping is= %d , %d", num1, num2);
+
+    return 0;
+}
